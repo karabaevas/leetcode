a97b8cf45a227a6a4af4b18e89244aa8bee3660d
GrannyPython <karabaevmb@gmdil.com>
2022-12-26 21:00:32 +0300

    rework

--- src/easy/LongestCommonPrefix/Solution.java
+++ src/easy/LongestCommonPrefix/Solution.java
@@ -2,31 +2,17 @@ package easy.LongestCommonPrefix;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(new Solution().longestCommonPrefix(new String[]{"car"}));
+        System.out.println(new Solution().longestCommonPrefix(new String[]{"flower","flow","flight", "tratata"}));
     }
 
     public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
-            return "";
-        }
-        if (strs.length == 1) {
-            return strs[0];
-        }
-        String shortest = strs[0];
+        String test = strs[0];
         for (String str : strs) {
-            if (str.length() < shortest.length()) {
-                shortest = str;
+            while (str.indexOf(test) != 0){
+                test = test.substring(0, test.length()-1);
             }
         }
 
-        for (int i = 0; i < shortest.toCharArray().length; i++) {
-            for (String str : strs) {
-                if (str.charAt(i) != shortest.charAt(i)) {
-                    return shortest.substring(0, i);
-                }
-            }
-        }
-
-        return shortest;
+        return test;
     }
 }
