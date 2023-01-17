f568d18edb011614d600bd5c4e3ddfca877b51bf
karabaevas <karabaevmb@gmail.com>
2023-01-17 20:25:56 +0300

    day 5 easy 14:52

--- src/easy/plusone/Solution.java
+++ src/easy/plusone/Solution.java
@@ -5,22 +5,16 @@ import java.util.Arrays;
 class Solution {
     public int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
-            if(digits[i] == 9){
-                digits[i] = 0;
-                if (i == 0){
-                    int[] result = new int[digits.length+1];
-                    for (int i1 = 1; i1 < result.length; i1++) {
-                        result[i1] = 0;
-                    }
-                    result[0] = 1;
-                    return result;
-                }
-            } else {
-                digits[i] = digits[i] + 1;
-                return digits;
-            }
+           if (digits[i] != 9){
+               digits[i]++;
+               return digits;
+           }
+           digits[i] = 0;
         }
-        return digits;
+
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
+        return result;
     }
 
     public static void main(String[] args) {
