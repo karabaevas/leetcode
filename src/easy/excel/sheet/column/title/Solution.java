92f6bec190ab61cc678ede0ce1d9086f4e20adb4
karabaevas <karabaevmb@gmail.com>
2024-01-14 18:28:13 +0300

    1+ hour

---
+++ src/easy/excel/sheet/column/title/Solution.java
@@ -0,0 +1,24 @@
+package easy.excel.sheet.column.title;
+
+class Solution {
+    public String convertToTitle(int columnNumber) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while (columnNumber >= 1){
+            int index = (columnNumber-1) % alphabet.length();
+            stringBuilder.append(alphabet.charAt(index));
+
+            columnNumber = (columnNumber - (index+1)) / alphabet.length();
+        }
+
+        return stringBuilder.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().convertToTitle(701));
+    }
+}
+
+a
