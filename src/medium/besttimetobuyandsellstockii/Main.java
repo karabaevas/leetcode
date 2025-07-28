3cf85530b9ab10ce4b98cda1e8047893a120d575
karabaevas <karabaevmb@gmail.com>
2025-07-28 09:10:47 +0300

    day22 = 16 min medium

---
+++ src/medium/besttimetobuyandsellstockii/Main.java
@@ -0,0 +1,21 @@
+package medium.besttimetobuyandsellstockii;
+
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println();
+    }
+
+    public int maxProfit(int[] prices) {
+        int result = 0;x
+
+        for (int i = 0; i < prices.length-1; i++){
+            int diff = prices[i + 1] - prices[i];
+            if (diff>0){
+                result = result + diff;
+            }
+        }
+
+        return result;
+    }
+}
