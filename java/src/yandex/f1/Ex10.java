42a2072d86c29b7b201a58b7bb1305af319b8f45
karabaevas <karabaevmb@gmail.com>
2026-03-05 21:00:43 +0300

    5 day

---
+++ java/src/yandex/f1/Ex10.java
@@ -0,0 +1,33 @@
+package yandex.f1;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Ex10 main = new Ex10();
+
+
+    }
+}
+
+class NumArray {
+    int[] arr;
+    int[] prefix;
+
+    public NumArray(int[] nums) {
+        arr = nums;
+        int n = nums.length;
+        prefix = new int[n];
+
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return prefix[right];
+        }
+        return prefix[right] - prefix[left - 1];
+    }
+}
