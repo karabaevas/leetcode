17f0de94b54cbe10c79df871db1f1e3a27e39d34
karabaevas <karabaevmb@gmail.com>
2026-03-14 21:15:19 +0300

    13 day

---
+++ java/src/yandex/blind75/Ex20.java
@@ -0,0 +1,39 @@
+package yandex.blind75;
+
+public class Ex20 {
+    public static void main(String[] args) {
+        Ex20 main = new Ex20();
+
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
