9336f962c54030c66afa1161a315ddb61a34a7c6
karabaevas <karabaevmb@gmail.com>
2026-03-23 22:53:21 +0300

    22 day

---
+++ java/src/yandex/last6month/Ex34.java
@@ -0,0 +1,31 @@
+package yandex.last6month;
+
+import java.util.*;
+
+public class Ex34 {
+    public static void main(String[] args) {
+        Ex34 main = new Ex34();
+    }
+
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        int i = 0;
+        int j = 0;
+
+        List<int[]> result = new ArrayList<>();
+        while(i < firstList.length && j < secondList.length){
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            if(start <= end){
+                result.add(new int[] {start, end});
+            }
+            if(firstList[i][1]< secondList[j][1]){
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
