5bd0c2fdfec6dfc3241faa772b14ef0dda1b9725
karabaevas <karabaevmb@gmail.com>
2026-04-23 22:41:09 +0300

    day 51

---
+++ java/src/leetcode/Ex228_1.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex228_1 {
+    public List<String> summaryRanges(int[] nums) {
+        int i = 0;
+        List<String> res = new ArrayList<>();
+
+        while(i < nums.length){
+            int start = nums[i];
+
+            while(i < nums.length-1 && nums[i]+1 == nums[i+1]){
+                i++;
+            }
+
+            if(nums[i] == start){
+                res.add("" + start);
+            } else {
+                res.add("" + start + "->" + nums[i]);
+            }
+
+            i++;
+        }
+
+        return res;
+    }
+}
