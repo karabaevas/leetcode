a81858564324c7f35c4e9c72b183973568fe97dd
Копатыч <karabaevmb@gmail.com>
2026-04-28 23:14:54 +0300

    day 56

---
+++ java/src/leetcode/Ex101.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+public class Ex101 {
+    public static void main(String[] args) {
+        Ex101 main = new Ex101();
+
+
+
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return doCheck(root.left, root.right);
+    }
+
+    boolean doCheck(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if((left == null && right != null) || (left != null && right == null)){
+            return false;
+        }
+
+        if(left.val == right.val){
+            return doCheck(left.left, right.right) && doCheck(left.right, right.left);
+        } else {
+            return false;
+        }
+    }
+
+
+}
