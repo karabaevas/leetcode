9afb3e5be360af3f03bf36fce0407d8319d05276
Копатыч <karabaevmb@gmail.com>
2026-05-10 22:04:32 +0300

    day 67

---
+++ java/src/leetcode/Ex724.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+public class Ex724 {
+    public static void main(String[] args) {
+        Ex724 main = new Ex724();
+
+
+
+    }
+
+    public int pivotIndex(int[] nums) {
+        int[] leftsum = new int[];
+        int[] rightsum = new int[];
+
+        int sum = 0;
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+            leftsum[i] = sum;
+        }
+
+        sum = 0;
+        for(int i = nums.length-1; i>=0; i--){
+            sum += nums[i];
+            rightsum[i] = sum;
+        }
+
+        for(int i = 0; i<nums.length; i++){
+            int left = 0;
+            int right = 0;
+            if(i != 0){
+                left = leftsum[i-1];
+            }
+
+            if(i != nums.length-1){
+                right = rightsum[i+1];
+            }
+
+            if(left == right)
+                return i;
+        }
+
+        return -1;
+    }
+
+}
