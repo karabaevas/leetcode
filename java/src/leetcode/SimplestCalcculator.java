df64fa26716da5812e6818df303e964c52ea29eb
Копатыч <karabaevmb@gmail.com>
2026-05-26 11:19:02 +0300

    day 80

--- java/src/leetcode/SimplestCalcculator.java
+++ java/src/leetcode/SimplestCalcculator.java
@@ -4,45 +4,46 @@ public class SimplestCalcculator {
     public static void main(String[] args) {
         SimplestCalcculator main = new SimplestCalcculator();
 
-
+        System.out.println(main.m("2+3*4+5")); ;
 
     }
-//    1
+
+    //    1
 //    102
 //    1 + 1
-//    3 * 4 * 5 + 2
+//    3 * 4 + 2
 //    Дана строка-выражение вида: "1+3*8+379*43". При этом:
     public int m(String str) {
         int res = 0;
-        int curNum = 0;
-        int multRes = 1;
+        int cur = 0;
+        int multiRes = 1;
         char lastSign = '+';
 
-        for(int i = 0; i < str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-            if(Character.isDigit(ch)){
-                curNum = curNum * 10 + (ch-'0');
+            if (Character.isDigit(ch)) {
+                cur = cur * 10 + (ch - '0');
             }
 
-            if(i == str.length()-1 || !Character.isDigit(ch)){
-                if(i == str.length()-1 || ch == '+'){
+            if (i == str.length()-1 ||  !Character.isDigit(ch)) {
+                if (i == str.length()-1 || ch == '+') {
                     if(lastSign == '*'){
-                        multRes *= curNum;
-                        curNum = 0;
-                        res = res + multRes;
-                        multRes = 1;
+                        multiRes *= cur;
+                        res += multiRes;
+                        cur = 0;
                     }
 
-                    res = res + curNum;
-                    curNum = 0;
+                    res += cur;
+                    cur = 0;
                     lastSign = '+';
-                } else {
-                    multRes *= curNum;
-                    curNum = 0;
+                } else if (ch == '*') {
+                    multiRes *= cur;
                     lastSign = '*';
+                    cur = 0;
                 }
             }
         }
-        return res;
+
+        return  res;
     }
 }
