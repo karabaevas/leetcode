7cd19c63002678ad3f019fcc628da90ba3dbf859
Копатыч <karabaevmb@gmail.com>
2026-05-31 11:02:12 +0300

    day 85

--- java/src/leetcode/Ex652.java
+++ java/src/leetcode/Ex652.java
@@ -1,44 +1,54 @@
 package leetcode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
+class TNode {
+    char val = '\0';  // [A-Z]
+    TNode left = null;
+    TNode right = null;
+};
+
 public class Ex652 {
-    public static void main(String[] args) {
-        Ex652 main = new Ex652();
+    Map<Integer, TNode> map = new HashMap<>();
 
 
+    public TNode findDuplicateSubtrees(TNode root) {
+        getMask(root);
 
-    }
-
-    Map<String, Integer> map = new HashMap<>();
-    List<TreeNode> res = new ArrayList<>();
-
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        if(root == null){
-            return null;
-        }
-
-        String key = toString(root);
-        int counter = map.getOrDefault(key , 0);
-        if(counter == 1){
-            res.add(root);
-        }
-        counter++;
-        map.put(key, counter);
-
-        findDuplicateSubtrees(root.left);
-        findDuplicateSubtrees(root.right);
         return res;
     }
 
-    String toString(TreeNode root){
-        if(root == null){
-            return "#";
-        }
-        return "v="+root.val + "l=" + toString(root.left) + "r=" + toString(root.right);
-    }
+    TNode res = null;
 
+    int getMask(TNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int lmask = getMask(root.left);
+        int rmask = getMask(root.right);
+
+        int mask = 1 << (root.val - 'A') | lmask | rmask;
+
+        if(map.get(mask) != null){
+            res = root;
+        }
+        map.put(mask, root);
+
+        return mask;
+    }
 }
+
+/*
+Дано бинарное дерево с выделенным корнем, в каждой вершине которого записано по одной букве A-Z.
+Две вершины считаются эквивалентными, если поддеревья этих вершин содержат одинаковое множество (т.е. без учета частот) букв.
+нужно найти любую пару эквивалентных вершин.
+      A
+    /   \
+   C     B
+  / \   / \
+  A D   A  D
+ /          \
+B            C
+*/
