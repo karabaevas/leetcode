4f718039b6a7ab0d73b14f8dea73d11c71832239
Копатыч <karabaevmb@gmail.com>
2026-06-01 12:36:51 +0300

    day 86

--- java/src/leetcode/Ex236.java
+++ java/src/leetcode/Ex236.java
@@ -4,24 +4,52 @@ public class Ex236 {
     public static void main(String[] args) {
         Ex236 main = new Ex236();
 
-
-
     }
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == p || root == q || root == null){
-            return root;
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root == p || root == q || root == null){
+//            return root;
+//        }
+//
+//        TreeNode left  = lowestCommonAncestor(root.left, p, q);
+//        TreeNode right = lowestCommonAncestor(root.right, p, q);
+//
+//        if(right != null && left != null){
+//            return root;
+//        }
+//
+//        return right == null? left : right;
+//    }
+
+    /**
+     * Найти ближайшего общего предка двух узлов дерева
+     * Ограничение по памяти О(1)
+     */
+
+    record TreeNode(
+            TreeNode parent,
+            TreeNode left,
+            TreeNode right
+    ){}
+
+    public TreeNode lowestCommonAncestor(TreeNode first, TreeNode second) {
+        TreeNode f = first;
+        TreeNode s = second;
+
+        while(f != s){
+            if(f == null){
+                f = second;
+            } else {
+                f = f.parent;
+            }
+
+            if(s == null){
+                s = first;
+            } else {
+                s = s.parent;
+            }
         }
 
-        TreeNode left  = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
-        if(right != null && left != null){
-            return root;
-        }
-
-        return right == null? left : right;
+        return f;
     }
-
-
 }
