7d285d141e73e92f99787a138d30b72ac253ce5c
Копатыч <karabaevmb@gmail.com>
2026-06-03 10:59:12 +0300

    day 88

---
+++ java/src/leetcode/Ex122.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class Ex122 {
+    public static void main(String[] args) {
+        Ex122 main = new Ex122();
+
+
+
+    }
+
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        for(int i = 1; i< prices.length; i++){
+            if(prices[i-1]< prices[i]){
+                res += prices[i] - prices[i-1];
+            }
+        }
+
+        return res;
+    }
+
+
+}
