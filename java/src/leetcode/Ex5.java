39ad319d7f3b09d1ff193fc06aa30812e907b5be
Копатыч <karabaevmb@gmail.com>
2026-06-06 18:58:41 +0300

    day 90

--- java/src/leetcode/Ex5.java
+++ java/src/leetcode/Ex5.java
@@ -42,4 +42,34 @@ public class Ex5 {
         return res;
     }
 
+
+    public String longestPalindromeDP(String s) {
+        String res = s.substring(0, 1);
+
+        int[][] dp = new int[s.length()][s.length()];
+
+        for(int i = 0; i< s.length(); i++){
+            dp[i][i] = 1;
+        }
+
+        for(int i = 0; i<s.length()-1; i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                dp[i][i+1] = 2;
+                res = s.substring(i,i+2);
+            }
+        }
+
+
+        for(int len = 3; len <= s.length(); len++){
+            for(int i = 0; i<=s.length()-len; i++){
+                int j = i + len - 1;
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] != 0){
+                    dp[i][j] = dp[i+1][j-1]+2;
+                    res = s.substring(i, j+1);
+                }
+            }
+        }
+
+        return res;
+    }
 }
