29cb4bf653c9b5ff4bcebfb2eb63d365c5578802
Копатыч <karabaevmb@gmail.com>
2026-06-20 16:30:21 +0300

    bt 1

---
+++ java/src/neetcode150/Ex53.java
@@ -0,0 +1,21 @@
+package neetcode150;
+
+public class Ex53 {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, maxSum = Integer.MIN_VALUE;
+        int left = 0, right = 0;
+
+        while(right < nums.length) {
+            while(right < nums.length && sum >= 0){
+                sum += nums[right];
+                maxSum = Math.max(sum, maxSum);
+                right++;
+            }
+
+            left = right;
+            sum = 0;
+        }
+
+        return maxSum;
+    }
+}
