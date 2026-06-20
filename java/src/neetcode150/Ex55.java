6ef4fd5f2cec1618ec2aefd72cc3589772f491c2
Копатыч <karabaevmb@gmail.com>
2026-06-20 19:54:31 +0300

    bt 1

---
+++ java/src/neetcode150/Ex55.java
@@ -0,0 +1,32 @@
+package neetcode150;
+
+public class Ex55 {
+    public boolean canJump(int[] nums) {
+        Boolean[] dp = new Boolean[nums.length];
+
+        return check(nums, 0, dp);
+    }
+
+    boolean check(int[] nums, int curIdx, Boolean[] dp){
+        if(curIdx == nums.length-1){
+            return true;
+        }
+
+        int curVal = nums[curIdx];
+        if(curVal == 0){
+            return false;
+        }
+
+        for(int i = 1; i<= curVal && curIdx + i < nums.length; i++){
+            boolean res = dp[curIdx+i] == null ?  check(nums, curIdx+i, dp) : false;
+
+            if(res){
+                return res;
+            } else {
+                dp[curIdx] = false;
+            }
+        }
+
+        return false;
+    }
+}
