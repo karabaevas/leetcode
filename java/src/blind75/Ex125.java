85aa359d8c29938584000cafacecea21730f1aa6
Копатыч <karabaevmb@gmail.com>
2026-06-26 16:10:18 +0300

    bt 7

---
+++ java/src/blind75/Ex125.java
@@ -0,0 +1,29 @@
+package blind75;
+
+public class Ex125 {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+
+    }
+
+}
