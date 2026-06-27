ef6337cbc988b998c9062ddc89ef53f4bbdfe13f
Копатыч <karabaevmb@gmail.com>
2026-06-27 22:06:17 +0300

    bt 8

---
+++ java/src/blind75/Ex206.java
@@ -0,0 +1,22 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex206 {
+//    public ListNode reverseList(ListNode head) {
+//
+//        ListNode cur = head;
+//
+//        while(cur != null){
+//            ListNode tmp = cur.next;
+//            cur.next = cur.prev;
+//            cur.prev = tmp;
+//            head = cur;
+//            cur = cur.prev;
+//        }
+//
+//        return head;
+//
+//    }
+}
