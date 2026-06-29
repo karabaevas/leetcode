68a3708e383d8a143d2b14430e615678abe33c5c
Копатыч <karabaevmb@gmail.com>
2026-06-29 08:26:55 +0300

    bt 10

---
+++ java/src/blind75/Ex21.java
@@ -0,0 +1,38 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex21 {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        ListNode h1 = list1;
+//        ListNode h2 = list2;
+//        ListNode head = new ListNode(0);
+//        ListNode dummy = head;
+//
+//        while(h1 != null && h2 != null){
+//            if(h1.val < h2.val){
+//                head.next = h1;
+//                h1 = h1.next;
+//            } else {
+//                head.next = h2;
+//                h2 = h2.next;
+//            }
+//
+//            head = head.next;
+//        }
+//
+//        while(h1 != null){
+//            head.next = h1;
+//            h1 = h1.next;
+//            head = head.next;
+//        }
+//        while(h2 != null){
+//            head.next = h2;
+//            h2 = h2.next;
+//            head = head.next;
+//        }
+//
+//        return dummy.next;
+//    }
+}
