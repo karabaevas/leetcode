21c41bba85cc4c50b6572e6c4afec0d25a7754f2
Копатыч <karabaevmb@gmail.com>
2026-06-29 08:11:31 +0300

    bt 10

---
+++ java/src/blind75/Ex141.java
@@ -0,0 +1,25 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex141 {
+//    public boolean hasCycle(ListNode head) {
+//        if(head == null || head.next == null){
+//            return false;
+//        }
+//
+//        ListNode slow = head;
+//        ListNode fast = head.next;
+//
+//        while(slow != fast){
+//            if(fast.next == null || fast.next.next == null){
+//                return false;
+//            }
+//
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//        return true;
+//    }
+}
