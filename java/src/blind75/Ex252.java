0b31456547305193f696b659d6daabc9b120fa26
Копатыч <karabaevmb@gmail.com>
2026-07-02 20:08:05 +0300

    bt 13

---
+++ java/src/blind75/Ex252.java
@@ -0,0 +1,21 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex252 {
+//    public boolean canAttendMeetings(List<Interval> intervals) {
+//        Collections.sort(intervals, Comparator.comparingInt(interval -> interval.start));
+//
+//        int cur = 0;
+//        for(Interval interval: intervals){
+//            if(cur <= interval.start){
+//                cur = interval.end;
+//            } else {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+}
