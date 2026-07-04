3a2d2e4ea50e242a44c1e3f686c716f4511efe09
Копатыч <karabaevmb@gmail.com>
2026-07-04 17:14:59 +0300

    bt 15

---
+++ java/src/blind75/Ex235.java
@@ -0,0 +1,28 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex235 {
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root == null){
+//            return null;
+//        }
+//
+//        if(root.val == p.val || root.val == q.val){
+//            return root;
+//        }
+//
+//        TreeNode lres = lowestCommonAncestor(root.left, p, q);
+//        TreeNode rres = lowestCommonAncestor(root.right, p, q);
+//
+//        if(lres != null && rres != null){
+//            return root;
+//        }
+//        if(lres != null){
+//            return lres;
+//        } else {
+//            return rres;
+//        }
+//    }
+}
