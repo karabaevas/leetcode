01db3baed89eec9abea88047a9f6b7578351e686
Копатыч <karabaevmb@gmail.com>
2026-07-04 18:01:49 +0300

    bt 15

---
+++ java/src/blind75/Ex98.java
@@ -0,0 +1,20 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex98 {
+//    public boolean isValidBST(TreeNode root) {
+//        return check(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+//    }
+//
+//    public boolean check(TreeNode root, int min, int max){
+//        if(root == null) return true;
+//
+//        if(!(root.val > min && root.val < max) ){
+//            return false;
+//        }
+//
+//        return check(root.left, min, root.val) && check(root.right, root.val, max);
+//    }
+}
