2e2cac29b0394e4bb340bc875b89df3efc32f3b4
Копатыч <karabaevmb@gmail.com>
2026-07-05 12:31:56 +0300

    bt 16

---
+++ java/src/blind75/Ex230.java
@@ -0,0 +1,29 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex230 {
+//    int counter = 0;
+//    int answer = 0;
+//
+//    public int kthSmallest(TreeNode root, int k) {
+//        goDeeper(root, k);
+//        return answer;
+//    }
+//
+//    public void goDeeper(TreeNode root, int k){
+//        if(root == null || counter == k){
+//            return;
+//        }
+//
+//        goDeeper(root.left, k);
+//
+//        if(++counter == k){
+//            answer = root.val;
+//            return;
+//        }
+//
+//        goDeeper(root.right, k);
+//    }
+}
