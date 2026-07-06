8b0d298112f264bb294b73d2a3dc1f1b3104a46b
Копатыч <karabaevmb@gmail.com>
2026-07-06 21:20:49 +0300

    bt 17

---
+++ java/src/blind75/Ex1929.java
@@ -0,0 +1,16 @@
+package blind75;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex1929 {
+    public int[] getConcatenation(int[] nums) {
+        int[] res = new int[nums.length*2];
+
+        for(int i = 0; i< nums.length*2; i++){
+            res[i] = nums[i%nums.length];
+        }
+
+        return res;
+    }
+}
